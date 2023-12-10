b67d1498a7bdc9c2da3f2b8adf0519f0663519f5
Evan van Urk <evan@skrepr.com>
2023-12-10 08:08:47 +0100

    Rewrite Map2D find funcs and add findAll funcs

--- src/Util/Map2D.php
+++ src/Util/Map2D.php
@@ -46,24 +46,72 @@ class Map2D
     }
 
     /**
-     * Walks the puzzle left to right then top to bottom and executes a
-     * callback for each position. Reverses direction if `$reverse` is `true`.
+     * Walks the map left to right then top to bottom and executes a callback for
+     * each position. Reverses direction if `$reverse` is `true`.
      *
-     * @param callable(int $x, int $y, ?string $value): void $callback
+     * @param callable(int $x, int $y, ?string $value): bool $callback
+     * Callback to execute. May return `true` to immediately stop walking the map.
+     * Should return false or void to only stop the current callback.
      */
     public function walk(callable $callback, bool $reverse = false): void
     {
-        foreach (Util::range(0, $this->h - 1, $reverse) as $y) {
-            foreach (Util::range(0, $this->w - 1, $reverse) as $x) {
-                $callback($x, $y, $this->get($x, $y));
+        $this->walkRegion(0, $this->w - 1, 0, $this->h - 1, $callback, $reverse);
+    }
+
+    /**
+     * Walks a region of the map left to right then top to bottom and executes a
+     * callback for each position. Reverses direction if `$reverse` is `true`.
+     *
+     * @param callable(int $x, int $y, ?string $value): bool $callback
+     * Callback to execute. May return `true` to immediately stop walking the map.
+     * Should return false or void to only stop the current callback.
+     */
+    public function walkRegion(
+        int $xMin,
+        int $xMax,
+        int $yMin,
+        int $yMax,
+        callable $callback,
+        bool $reverse = false,
+    ): void {
+        foreach (Util::range($yMin, $yMax, $reverse) as $y) {
+            foreach (Util::range($xMin, $xMax, $reverse) as $x) {
+                if (true === $callback($x, $y, $this->get($x, $y))) {
+                    break 2;
+                }
             }
         }
     }
 
     /**
-     * Searches a region of the map for the given search term or expression.
-     * Searches from left to right then top to bottom by default, or from right
-     * to left then bottom to top if `$reverseSearch` is `true`.
+     * Searches the map for the given search term or expression and returns the
+     * first match. Searches from left to right then top to bottom by default,
+     * or from right to left then bottom to top if `$reverseSearch` is `true`.
+     *
+     * @return null|array{'x': int, 'y': int} Array with coordinates of first
+     * match. Null if search was not found or matched.
+     */
+    public function find(
+        string $search,
+        bool $regexp = false,
+        bool $reverseSearch = false
+    ): ?array {
+        return $this->findInRegion(
+            0,
+            $this->w - 1,
+            0,
+            $this->h - 1,
+            $search,
+            $regexp,
+            $reverseSearch
+        );
+    }
+
+    /**
+     * Searches a region of the map for the given search term or expression and
+     * returns the first match. Searches from left to right then top to bottom
+     * by default, or from right to left then bottom to top if `$reverseSearch`
+     * is `true`.
      *
      * @return null|array{'x': int, 'y': int} Array with coordinates of first
      * match. Null if search was not found or matched.
@@ -77,18 +125,85 @@ class Map2D
         bool $regexp = false,
         bool $reverseSearch = false,
     ): ?array {
-        foreach (Util::range($yMin, $yMax, $reverseSearch) as $y) {
-            foreach (Util::range($xMin, $xMax, $reverseSearch) as $x) {
-                $value = $this->get($x, $y);
-                if ($value === null) { continue; }
+        $match = null;
+        $this->walkRegion(
+            $xMin, $xMax, $yMin, $yMax,
+            function(int $x, int $y, ?string $value) use ($search, $regexp, &$match) {
+                if ($value === null) { return false; }
+                if (is_numeric($value)) { $value = (string) $value; }
 
-                if ((!$regexp && $this->get($x, $y) === $search)
+                if ((!$regexp && $value === $search)
                     || ($regexp && preg_match($search, $value))) {
-                    return ['x' => $x, 'y' => $y];
+                    $match = ['x' => $x, 'y' => $y];
+                    return true;
+                }
+
+                return false;
+            },
+            $reverseSearch
+        );
+
+        return $match;
+    }
+    /**
+     * Searches the map for the given search term or expression and returns all
+     * matches. Searches from left to right then top to bottom by default, or
+     * from right to left then bottom to top if `$reverseSearch` is `true`.
+     *
+     * @return array<string, array{'x': int, 'y': int}> Coordinates per match
+     * indexed by value. Empty if search was not found or matched.
+     */
+    public function findAll(
+        string $search,
+        bool $regexp = false,
+        bool $reverseSearch = false
+    ): array {
+        return $this->findAllInRegion(
+            0,
+            $this->w - 1,
+            0,
+            $this->h - 1,
+            $search,
+            $regexp,
+            $reverseSearch
+        );
+    }
+
+    /**
+     * Searches a region of the map for the given search term or expression and
+     * returns all matches. Searches from left to right then top to bottom by
+     * default, or from right to left then bottom to top if `$reverseSearch`
+     * is `true`.
+     *
+     * @return array<string, array{'x': int, 'y': int}> Coordinates per match.
+     * indexed by value. Empty if search was not found or matched.
+     */
+    public function findAllInRegion(
+        int $xMin,
+        int $xMax,
+        int $yMin,
+        int $yMax,
+        string $search,
+        bool $regexp = false,
+        bool $reverseSearch = false,
+    ): array {
+        $matches = [];
+        $this->walkRegion(
+            $xMin, $xMax, $yMin, $yMax,
+            function(int $x, int $y, ?string $value) use (&$matches, $search, $regexp) {
+                if ($value === null) { return; }
+                if (is_numeric($value)) { $value = (string) $value; }
+
+                if ((!$regexp && $value === $search)
+                    || ($regexp && preg_match($search, $value))) {
+                    if (false === array_key_exists($value, $matches)) {
+                        $matches[$value] = [];
+                    }
+                    $matches[$value][] = ['x' => $x, 'y' => $y];
                 }
             }
-        }
+        );
 
-        return null;
+        return $matches;
     }
 }
